600d943d9b735e70a8b197e405a9edd96bbd9dba
Ireneeer <irene.ferrente@mail.polimi.it>
2024-04-26 16:11:11 +0200

    ChatMessage

---
+++ AM20/src/main/java/it/polimi/ingsw/controller/messages/ChatMessage.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.controller.messages;
+
+public class ChatMessage extends Message{
+
+    private String sender;
+    private String recipient;
+    private String message;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
