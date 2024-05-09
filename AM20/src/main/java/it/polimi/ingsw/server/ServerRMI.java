d2b87ccb533426db5f4c84e87fb9205a774338ef
EleonoraFicarelli <eleonora.ficarelli@mail.polimi.it>
2024-05-09 15:54:04 +0200

    javadoc server RMI & SKT

--- AM20/src/main/java/it/polimi/ingsw/server/ServerRMI.java
+++ AM20/src/main/java/it/polimi/ingsw/server/ServerRMI.java
@@ -18,6 +18,9 @@ import java.util.Map;
 //classe che implementa la ricezione delle azioni dalla classe RMI
 public class ServerRMI implements Loggable{
 
+    /**
+     * this attribute stands as a reference to the serverManager
+     */
     //per ogni utente dice se è connesso con RMI o SOCKET
     private ServerManager manager;
 
@@ -25,6 +28,15 @@ public class ServerRMI implements Loggable{
         this.manager = manager;
     }
 
+    //TODO: RIGUARDARE
+    /**
+     * This method actually allows the client "client" to join the game
+     * @param client the client that's joining the game
+     * @param callback client's reference that allows the server to contact the client when something significant happens
+     * @return a connectionAckMessage
+     * @throws RemoteException
+     * @throws CannotJoinGameException
+     */
     @Override
     public  ConnectionAckMessage login(String client, Connection callback) throws RemoteException, CannotJoinGameException {
         HashMap<String, ConnectionAckMessage> res;
@@ -47,12 +59,33 @@ public class ServerRMI implements Loggable{
         return res.get(client);
     }
 
+    //TODO: RIGUARDARE
+    /**
+     * This method actually allows, to the client that want to start a new game, to start a new game
+     * @param client the client that just started the game, the one that created it
+     * @param numPlayers the number of players that the client "client" want to play with
+     * @param callback client's reference that allows the server to contact the client when something significant happens
+     * @return a message to the client "client"
+     * @throws RemoteException
+     * @throws InvalidArgumentException
+     * @throws InvalidPlayingException
+     */
     @Override
     public Message startNewGame(String client, int numPlayers, Connection callback) throws RemoteException, InvalidArgumentException, InvalidPlayingException {
         this.manager.getConnections().put(client, callback);
         return this.manager.getController().newGame(client,numPlayers);
     }
 
+    //TODO: RIGUARDARE
+    /**
+     * This method actually allows the player that has "nickname" as their nickname to choose the starter card side.
+     * @param nickname the nickname of the player that's choosing the side of the card
+     * @param side the side of the card that the player's just chosen (could only be front or back)
+     * @return a starterCardAckMessage to the player that's just chosen the side
+     * @throws RemoteException
+     * @throws InvalidArgumentException
+     * @throws InvalidPlayingException
+     */
     @Override
     public StarterCardAckMessage chooseStarterCardSide(String nickname, int side) throws RemoteException, InvalidArgumentException, InvalidPlayingException {
         HashMap<String, StarterCardAckMessage> res;
@@ -70,6 +103,18 @@ public class ServerRMI implements Loggable{
         return res.get(nickname);
     }
 
+    //TODO: RIGUARDARE
+    /**
+     * This method actually allows the player that has "nickname" as their nickname to choose their secret objective
+     * between the 2 possible objectives.
+     * @param nickname the nickname of the player that's choosing the side of the card
+     * @param index the index, of the 2 items' array that contains the two possible secret objectives, of the specific
+     *              secret objective that's been choosen.
+     * @return an objectiveAckMessage to the player that's just chosen the objective
+     * @throws RemoteException
+     * @throws InvalidArgumentException
+     * @throws InvalidPlayingException
+     */
     @Override
     public ObjectiveAckMessage chooseObjective(String nickname, int index) throws RemoteException, InvalidArgumentException, InvalidPlayingException {
         HashMap<String, ObjectiveAckMessage> res;
@@ -87,6 +132,24 @@ public class ServerRMI implements Loggable{
         return res.get(nickname);
     }
 
+    //TODO: RIGUARDARE
+    /**
+     * This method actually allows the player that has "playerNickname" as their nickname to play the card that's found
+     * in the "cardIndex" position of the 3 items' array of their own cards.
+     * @param playerNickname the nickname of the player that's playing the card
+     * @param cardIndex the specific index of the card that the player wants to play now
+     * @param angle the angle of the targetID card is going to be covered with the card that is being played
+     * @param targetID the ID of the card that the player wants to cover by playing their card
+     * @param side this attribute stands for the side of the card that's just been played (it could be front or back only)
+     * @return an AcknowledgeMessage to the player that's just played the card
+     * @throws RemoteException
+     * @throws InvalidArgumentException
+     * @throws RequirementsNotRespectedException
+     * @throws InvalidPlayingException
+     * @throws TargetNotPresentException
+     * @throws InvalidAngleCoveredException
+     * @throws InvalidPositionException
+     */
     @Override
     public AcknowledgeMessage playCard(String playerNickname, int cardIndex, int angle, String targetID, int side)
             throws RemoteException, InvalidArgumentException, RequirementsNotRespectedException,
@@ -113,7 +176,19 @@ public class ServerRMI implements Loggable{
         }
         return res.get(playerNickname);
     }
+    //TODO: RIGUARDARE
 
+    /**
+     * This method actually allows the player that has "playerNickname" as their nickname to pick a card from the specific
+     * "deck".
+     * @param playerNickname the nickname of the player that needs to pick a card
+     * @param deck the specific deck where the player is picking up the card from (could only be gold or resource)
+     * @return an AcknowledgeMessage to the player that's just picked the card
+     * @throws RemoteException
+     * @throws InvalidArgumentException
+     * @throws InvalidPlayingException
+     * @throws FinishedCardStackException
+     */
     @Override
     public AcknowledgeMessage pickCard(String playerNickname, int deck)
             throws RemoteException, InvalidArgumentException, InvalidPlayingException,
@@ -137,6 +212,19 @@ public class ServerRMI implements Loggable{
         return res.get(playerNickname);
     }
 
+    //TODO: RIGUARDARE
+    /**
+     * This method actually allows the player that has "playerNickname" as their nickname to pick the card that's found
+     * in the index "index" of the "deck"'s visible cards.
+     * @param playerNickname the nickname of the player that needs to pick a card
+     * @param deck the specific deck where the player is picking up the card from (could only be gold or resource)
+     * @param index the index, of the 2 items' array, of the card that the player's chosen to pick up
+     * @return an AcknowledgeMessage to the player that's just picked the card
+     * @throws RemoteException
+     * @throws InvalidArgumentException
+     * @throws InvalidPlayingException
+     * @throws FinishedCardStackException
+     */
     @Override
     public AcknowledgeMessage pickCard(String playerNickname, int deck, int index)
             throws RemoteException, InvalidArgumentException, InvalidPlayingException,
@@ -159,7 +247,15 @@ public class ServerRMI implements Loggable{
         }
         return res.get(playerNickname);
     }
-
+    //TODO: RIGUARDARE
+    /**
+     * This method allows the player "sender" to actually send a message to the "recipient" that contains "message" in it.
+     * @param sender the nickname of the player that's sending a message
+     * @param recipient the nickname of the player that's receiving the message
+     * @param message the actual message
+     * @return a Message to the "recipient" player
+     * @throws RemoteException
+     */
     @Override
     public Message sendChatMessage(String sender, String recipient, String message) throws RemoteException {
         ChatMessage msg = new ChatMessage(sender, recipient, message);
@@ -172,7 +268,14 @@ public class ServerRMI implements Loggable{
         }
         return m;
     }
+    //TODO: RIGUARDARE
 
+    /**
+     * This method allows the sender player to send a message to all other players.
+     * @param sender the nickname of the player that's sending the message
+     * @param message the actual message
+     * @return  ...
+     */
     @Override
     public Message sendBroadcastChatMessage(String sender, String message) {
         BroadcastChatMessage msg = new BroadcastChatMessage(sender, message);
