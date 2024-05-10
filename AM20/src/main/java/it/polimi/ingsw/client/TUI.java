cb5a23dd8ad896a85e14e9e4a09e8fc0e4a54f60
Mattia Doro <100339042+mado002@users.noreply.github.com>
2024-05-10 10:13:35 +0200

    tui controller

--- AM20/src/main/java/it/polimi/ingsw/client/TUI.java
+++ AM20/src/main/java/it/polimi/ingsw/client/TUI.java
@@ -29,7 +29,6 @@ public class TUI implements UIManager {
     @Override
     public void updateCards(List<PlayableCard> cards) {
         this.cards = cards;
-        printCards();
     }
 
     @Override
@@ -82,9 +81,77 @@ public class TUI implements UIManager {
         this.starterCard = starterCard;
     }
 
-    public void printCards(){
-        for(PlayableCard card : cards){
-            System.out.println(card.getCardKingdom());
+
+    public void printTitle(){
+        System.out.println("   ____          _             _   _       _                   _ _     ");
+        System.out.println("  / ___|___   __| | _____  __ | \\ | | __ _| |_ _   _ _ __ __ _| (_)___ ");
+        System.out.println(" | |   / _ \\ / _` |/ _ \\ \\/ / |  \\| |/ _` | __| | | | '__/ _` | | / __|");
+        System.out.println(" | |__| (_) | (_| |  __/>  <  | |\\  | (_| | |_| |_| | | | (_| | | \\__ \\");
+        System.out.println("  \\____\\___/ \\__,_|\\___/_/\\_\\ |_| \\_|\\__,_|\\__|\\__,_|_|  \\__,_|_|_|___/");
+        System.out.println("                                                                       ");
+        System.out.println("\n");
+    }
+
+
+    public void clearTerminal(int linesToPreserve) {
+        String ANSI_RESET = "\u001B[0m";
+        String ANSI_CLEAR_SCREEN = "\u001B[2J";
+        String ANSI_MOVE_CURSOR_UP = "\u001B[%dA"; // Move cursor up by specified number of lines
+        // Move the cursor up by the specified number of lines
+        System.out.print(String.format(ANSI_MOVE_CURSOR_UP, linesToPreserve));
+        // Clear the screen
+        System.out.print(ANSI_CLEAR_SCREEN);
+        // Move the cursor to the beginning (optional)
+        System.out.print("\u001B[H");
+        System.out.flush();
+    }
+
+
+    public static void waitSeconds(int seconds) {
+        long startTime = System.currentTimeMillis();
+        long targetTime = startTime + seconds * 1000L;
+
+        while (System.currentTimeMillis() < targetTime) {
+            // Loop until the current time exceeds the target time
         }
+    }
+
+    public static void writeCommandParam( ){
+        System.out.println("The following lines explain which are the parameters for every action: \n");
+        System.out.println("Reminder: ypu don't need to type the + but just a space between parameters");
+        System.out.println("/join + username");
+        System.out.println("/newGame + username +  numPlayers");
+        System.out.println("/chooseObjective +  index  ");
+        System.out.println("/chooseStarterSide + side (0 for back / 1 for front)");
+        System.out.println("/pickCardDeck +  deck (0 for gold deck / 1 for resource deck)");
+        System.out.println("/pickCardVisible +  deck (0 for gold deck / 1 for resource deck) +  index  (0 for the left card and 1 for the right one)");
+        System.out.println("/playCard + index (index of the card you want to play) +  angle (angel of the card you want to cover: 0 for UL / 1 for UR / 2 for DL /3  for DR)+  targetIDcard (ID of the card you want to cover) + side (0 for back / 1 for front)");
+        System.out.println("/chat + broadCast + message ");
+        System.out.println("/chat + username (username of the reciever) + message");
+        System.out.println("/myPlayerInfo (to visualize your info)");
+        System.out.println("/playerInfo + username (usarname of the player you want to view)");
+        System.out.println("/placement (view the placement of the game)");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        TUI myTui = new TUI();
+        myTui.printTitle();
+
+        writeCommandParam();
+        myTui.waitSeconds(5);
+        myTui.clearTerminal(5);
+
+    }
+
+    public void viewPlayerInfo(){
+
+    }
+
+    public void viewOtherPlayerInfo(String username){
+
+    }
+
+    public void displayPlacement(){
+
     }
 }
--- AM20/src/main/java/it/polimi/ingsw/client/TUIController.java
+++ AM20/src/main/java/it/polimi/ingsw/client/TUIController.java
@@ -1,13 +1,10 @@
 package it.polimi.ingsw.client;
 
-import it.polimi.ingsw.server.ServerManager;
-import jdk.tools.jmod.Main;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.Socket;
-import java.nio.Buffer;
 
 public class TUIController extends ClientController implements Runnable{
 
@@ -19,43 +16,59 @@ public class TUIController extends ClientController implements Runnable{
 
     private BufferedReader br;
 
+    private TUI myTUI;
+
     public TUIController(Socket socket){
-        this.br = new BufferedReader(new jdk.internal.org.jline.utils.InputStreamReader(System.in));
+        this.br = new BufferedReader( new InputStreamReader(System.in));
         this.socket = socket;
     }
 
 
     public void run(){
         while(true){
-            System.out.println("Decidi che comando vuoi eseguire");
+            System.out.println("Decide which command you want to do:\n");
             try {
+
                 String input = br.readLine();
-                if(input.equals("/join")){
-                    this.join();
+                String[] command = input.split(" ");
+                if(command[0].equals("/join")){
+                    this.username = command[1];
+                    this.clientSender.login(input);
+                }else if(command[0].equals("/newGame")){
+                    this.username = command[1];
+                    this.clientSender.startNewGame(command[1], Integer.parseInt(command[2]));
 
-                }else if(input.equals("/newGame")){
-                    this.newGame();
+                }else if(command[0].equals("/chooseObjective")){
 
-                }else if(input.equals("/chooseObjective")){
-                    this.chooseObjective();
+                    this.clientSender.chooseObjective(this.username, Integer.parseInt(command[1]));
 
-                }else if(input.equals("/chooseStarterSide")){
-                    this.chooseStarterSide();
+                }else if(command[0].equals("/chooseStarterSide")){
+                    this.clientSender.chooseStarterCardSide(this.username, Integer.parseInt(command[1]));
 
-                }else if(input.equals("/pickCardDeck ")){
-                    this.pickCardDeck();
+                }else if(command[0].equals("/pickCardDeck")){
+                    this.clientSender.pickCard(this.username, Integer.parseInt(command[1]));
 
-                }else if(input.equals("/pickCardVisible")){
-                    this.pickCardVisible();
+                }else if(command[0].equals("/pickCardVisible")){
+                    this.clientSender.pickCard(this.username, Integer.parseInt(command[1]), Integer.parseInt(command[2]));
 
-                }else if(input.equals("/playCard")){
-                    this.playCard();
+                }else if(command[0].equals("/playCard")){
+                    this.clientSender.playCard(this.username, Integer.parseInt(command[1]), Integer.parseInt(command[2]), command[3], Integer.parseInt(command[4]));
 
-                }else if(input.equals("/chat")){
-                    this.chat();
-                }//else if(input.equals("/board")){
+                }else if(command[0].equals("/chat")){
+                    if(command[1]. equals("broadcast"))
+                        this.clientSender.sendBroadcastChatMessage(this.username, command[2]);
+                    else
+                        this.clientSender.sendChatMessage(this.username, command[1], command[2]);
 
-                //}
+                }else if(command[0].equals("/myPlayerInfo")){
+                    this.myTUI.viewPlayerInfo();
+
+                }else if(command[0].equals("/playerInfo")){
+                    this.myTUI.viewOtherPlayerInfo(command[1]);
+
+                }else if(command[0].equals("/placement")){
+                    this.myTUI.displayPlacement();
+                }
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -63,136 +76,6 @@ public class TUIController extends ClientController implements Runnable{
         }
 
     }
-
-    private void join(){
-        System.out.println("Per fare la join devi specificare il nickname con cui ti vuoi collegare alla partita:");
-        try {
-            String input = br.readLine();
-            this.username = input;
-            this.clientSender.login(input);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    private void newGame(){
-        System.out.println("Per creare un nuovo gioco devi speciificare il tuo nickname: ");
-        try {
-            String inputNickname = br.readLine();
-            this.username = inputNickname;
-            System.out.println("Ora devi speicifcare quantio gicoatori vuoi che ci siano colelgati alla partita: ");
-            String inputNum = br.readLine();
-            this.clientSender.startNewGame(inputNickname, Integer.parseInt(inputNum));
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    //TODO
-    //public perchè deve poter essere chiamato da RMIClientReceivere e SKTClientReceiver
-    public void chooseObjective(){
-        System.out.println("Specifica quale delle due carte obiettivo che hai ricevuto vuoi usare (0 per sinistra e 1 per destra)");
-
-        try {
-            String inputSecretObjective = br.readLine();
-            this.clientSender.chooseObjective(this.username, Integer.parseInt(inputSecretObjective));
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
-    }
-
-    //TODO
-    //public per lo stesso motivo di chooseObjective
-    public void chooseStarterSide(){
-        System.out.println("Specifica quale lato della carta iniziale vuoi giocare (0 per il dietro della carta e 1 per il fronte della carta): ");
-        try {
-            String inputSide = br.readLine();
-            this.clientSender.chooseStarterCardSide(this.username, Integer.parseInt(inputSide));
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void pickCardDeck(){
-
-        System.out.println("Specifica da quale deck si vuole pescare (0 per le gold card e 1 per le resource card): ");
-        try {
-            String inputDeck = br.readLine();
-            this.clientSender.pickCard(this.username, Integer.parseInt(inputDeck));
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    private void pickCardVisible(){
-
-        System.out.println("Specifica da quale deck si vuole pescare (0 per le gold card e 1 per le resource card): ");
-        try {
-            String inputDeck = br.readLine();
-            System.out.println("Specifica quale carta visibile vuoi pescare (0 oppure 1)");
-            String inputIndex = br.readLine();
-            this.clientSender.pickCard(this.username, Integer.parseInt(inputDeck), Integer.parseInt(inputIndex));
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
-    }
-
-
-
-    private void playCard(){
-
-        System.out.println("Specifica carta si vuole giocare (0 per quella più a sinistra, 1 per quella in mezzo e 2 per quella di destra): ");
-        try {
-            String inputCard = br.readLine();
-            System.out.println("Specifica su quale angolo vuoi posizionare la carta si vuole pescare (0 per UL, 1 per UR, 2 per DL e 3 per DR):");
-            String inputAngle = br.readLine();
-            System.out.println("Specifica su quale carta si vuole giocare la carta scelta: ");
-            String inputTargetcard = br.readLine();
-            System.out.println("Specifica con quale lato si vuole giocare la carta (0 per il dietro della carta e 1 per il fronte della carta): ");
-            String inputSide = br.readLine();
-            //metodo che ti peremtte di ottenre l'ID della carta da giocare: la TUA avrà HashMap<Integer, ID>
-            this.clientSender.playCard(this.username, Integer.parseInt(inputCard), Integer.parseInt(inputAngle), "S0", Integer.parseInt(inputSide));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
-    }
-
-    private void chat(){
-        System.out.println("Specifica se si vuole amdnare un messaggio in broadcast (scrivi /broadcast oppure l'username del ricevitore del messaggio");
-        try {
-            String decisione = br.readLine();
-            String messagge;
-            if(decisione.equals("/broadcast")){
-                System.out.println("Scrivi il messaggio che si vuole mandare: ");
-                messagge = br.readLine();
-                this.clientSender.sendBroadcastChatMessage(this.username, messagge);
-
-            }else {
-                System.out.println("Scrivi il messaggio che si vuole mandare: ");
-                messagge = br.readLine();
-                this.clientSender.sendChatMessage(this.username, decisione , messagge);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
 
 
 }
